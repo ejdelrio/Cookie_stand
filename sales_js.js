1112fe5d1bbdf528f947941dc63265850c3d88b1
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-05-15 15:24:49 -0700

    Created a for loop that contstructs objects using vanilla Js :D

--- sales_js.js
+++ sales_js.js
@@ -22,16 +22,42 @@ var stores = {
 var storeKeys = Object.keys(stores);
 
 for (var i = 0, l = storeKeys.length; i < l; i ++) {
-  stores[storeKeys[i]].minPerHour = mins[i];
-  stores[storeKeys[i]].maxPerHour = maxes[i];
-  stores[storeKeys[i]].avgPerCustomer = averages[i];
-  stores[storeKeys[i]].cusPerHour = function () {
-    return Math.floor(Math.random() * (this.maxPerHour - this.minPerHour) + this.maxPerHour);
+  var storeLocation = stores[storeKeys[i]];
+  storeLocation.minPerHour = mins[i];
+  storeLocation.maxPerHour = maxes[i];
+  storeLocation.avgPerCustomer = averages[i];
+  storeLocation.cusPerHour = function () {
+    return Math.floor(Math.random() * (this.maxPerHour - this.minPerHour) + this.minPerHour);
   },
-  stores[storeKeys[i]].cookiesPerHour = function () {
-    return this.cusPerHour * this.avgPerCustomer;
+  storeLocation.cookiesPerHour = function () {
+    return this.cusPerHour() * this.avgPerCustomer;
   },
-  stores[storeKeys[i]].dailySales = {
-    six: '',
-  }
+  storeLocation.dailySales = [],
+  storeLocation.salesGen = function () {
+    //Generates 14 random numbers to append to dailySales
+    var total = 0;
+    for (var n = 0; n < 14; n++) {
+      var hourlySales = this.cookiesPerHour();
+      this.dailySales.push(hourlySales);
+      total += hourlySales;
+    }
+    this.dailySales.push(total);
+  },
+  storeLocation.weeklySales = {
+    monday: '',
+    tuesday: '',
+    wednesday: '',
+    thursday: '',
+    friday: '',
+    saturday: '',
+    sunday: ''
+  },
+  storeLocation.weeklyGen = function () {
+    for (var key in this.weeklySales) {
+      this.salesGen();
+      this.weeklySales[key] = (this.dailySales);
+      this.dailySales = [];
+    }
+  };
+  storeLocation.weeklyGen();
 }
