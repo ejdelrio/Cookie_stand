e7927ff2b113f3796237de80ade760832ccee76b
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-05-15 18:02:22 -0700

    Began editing the inden.html

--- sales_js.js
+++ sales_js.js
@@ -1,10 +1,10 @@
 'use strict';
 //array of minimums sales in order of locations
-var mins = [23, 3, 11, 20, 2];
+var mins = [23, 3, 11, 20, 2, 30];
 //array of max sales in order of locations
-var maxes = [64, 28, 38, 38, 16];
+var maxes = [64, 28, 38, 38, 16, 12];
 //Array of average sales per customer in order
-var averages = [6.3, 1.2, 3.7, 2.3, 4.6];
+var averages = [6.3, 1.2, 3.7, 2.3, 4.6, 10];
 //object called stores contains a list of stores with their location as the key for each location
 var stores = {
   firstAndPike : {
@@ -16,7 +16,8 @@ var stores = {
   capitalHill: {
   },
   alki: {
-  }
+  },
+  NewYork: {}
 };
 //array of values from stores object
 var storeKeys = Object.keys(stores);
@@ -55,7 +56,7 @@ for (var i = 0, l = storeKeys.length; i < l; i ++) {
     sunday: ''
   },
   storeLocation.weeklyGen = function () {
-    //runs dailySales function 7 times and defines each array one as a weekly value
+    //runs dailySales function 7 times and defines each array one as a weeklySales value
     for (var key in this.weeklySales) {
       this.salesGen();
       this.weeklySales[key] = (this.dailySales);
@@ -67,6 +68,8 @@ for (var i = 0, l = storeKeys.length; i < l; i ++) {
 
 
 function postSales() {
+  //Sort through stores Objectes and creates sorted DOM elements for each
+  //location, day and hour with corresponding sales
   for (var keys in stores) {
     var storeDiv = document.createElement('div');
     var title = document.createElement('h1');
@@ -98,11 +101,13 @@ function postSales() {
     }
     document.body.appendChild(storeDiv);
   }
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < Object.keys(stores).length; i++) {
     document.getElementsByTagName('div')[i].style.display = 'inline-block';
     document.getElementsByTagName('div')[i].style.outline = '1px solid black';
     document.getElementsByTagName('div')[i].style.width = '200px';
   }
 }
+
+
 
 postSales();
