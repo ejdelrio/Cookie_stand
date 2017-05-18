c9ddef88783df5f11c6b7f1d8e6a363bcdd17cc2
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-05-18 16:03:38 -0700

    Still working on new form to change cell values

--- sales_js.js
+++ sales_js.js
@@ -6,6 +6,7 @@ var averages = [6.3, 1.2, 3.7, 2.3, 4.6];
 var names = ['First and Pike', 'Seatac Airport', 'Seattle Center', 'Capital Hill', 'Alki'];
 var openHour = 6; //var will be used to assign key value pairs, additionally, var will be uesed in for loops for DOM li elements
 var closeHour = 21;
+var objectCounter = 0;
 //Test array collects hourly totals per index and a daily total as the last index for our
 //total row
 var hourlyTotals = ['<td>Total:</td>'];
@@ -13,8 +14,12 @@ for (var i = 0; i < (closeHour - openHour) + 1; i++) {
   hourlyTotals.push(0);
 }
 
-var tableBody = document.getElementById('table_body');
-var form = document.getElementById('Store Entry');
+
+var tableBody = document.createElement('tbody');
+var form = document.getElementById('store_entry');
+var cellForm = document.getElementById('cell_select');
+
+
 
 function Store (storeLocation, minimum, maximum, averages) {
   //Store constructor that creates new store Objects
@@ -29,14 +34,18 @@ function Store (storeLocation, minimum, maximum, averages) {
   //Creates a week object that will house each daily sales array
 }
 
+
 Store.prototype.cusPerHour = function () {
   //generates a random number that is between the minPerHour and maxPerHour
   return Math.floor(Math.random() * (this.maxPerHour - this.minPerHour + 1) + this.minPerHour);
 };
 
+
 Store.prototype.cookiesPerHour = function () {
   return this.cusPerHour() * this.avgPerCustomer;
 };
+
+
 
 Store.prototype.salesGen = function () {
   //Generates random numbers to append to dailySales, 1 for every hour of operation and a total
@@ -51,6 +60,8 @@ Store.prototype.salesGen = function () {
   this.dailySales.push('<td>' + total + '</td>');
 };
 
+
+
 Store.prototype.render = function () {
   //Functions creates table row and appends joined dailSales array
   //table row is then appended to tableBody
@@ -59,16 +70,56 @@ Store.prototype.render = function () {
   tableBody.appendChild(storeRow);
 };
 
+function dropDownTime(newEntry, value, menuName) {
+  var dropMenu = document.getElementsByName(menuName)[0];
+  var menuItem = document.createElement('option');
+  menuItem.value = value;
+  menuItem.innerHTML = newEntry;
+  dropMenu.appendChild(menuItem);
+}
+
+
+
+
+function headGen() {
+  //Generates a table head that is variable and based off of opening and closing time
+  var headArray = ['<td>Locations</td>'];
+  var masterTable = document.getElementById('table_head');
+  var tHead = document.createElement('thead');
+
+  for (var i = 0; i < (closeHour - openHour); i++) {
+    if (i + openHour <= 12) {
+      headArray.push('<td>' + (i + openHour)  + ' am</td>');
+      dropDownTime((i + openHour)  + ' am', i+ 1, 'time');
+    } else {
+      headArray.push('<td>' + (i - openHour) + ' pm</td>');
+      dropDownTime((i - openHour)  + ' pm', i+ 1, 'time');
+    }
+  }
+  headArray.push('<td>Totals</td>');
+  tHead.innerHTML = headArray.join('');
+  masterTable.appendChild(tHead);
+  masterTable.appendChild(tableBody);
+}
+
+headGen();
+
+
+
 function alreadyExist() {
   //Generates predefined objects with array data
   for (var i = 0; i < names.length; i++) {
     var tempName = new Store(names[i], mins[i], maxes[i], averages[i]);
     tempName.salesGen();
     tempName.render();
+    dropDownTime(tempName.storeLocation, objectCounter, 'location');
+    objectCounter+= 1;
   }
 }
 
 alreadyExist();
+
+
 
 function totalGen () {
   //uses hourlyTotals to create a row of totals that is appended to table
@@ -80,8 +131,9 @@ function totalGen () {
   totalRow.innerHTML = totalArray.join('');
   tableBody.appendChild(totalRow);
 }
-
 totalGen();
+
+
 
 function createStore(e) {
   //prevents submit page from triggering html page reload
@@ -95,14 +147,35 @@ function createStore(e) {
   var tempStore = new Store(name, minimum, maximum, avg);
   //Deletes total row from table
   var rowLength = document.getElementsByTagName('tr').length;
-  document.getElementById('table_body').deleteRow(rowLength - 2);
+  tableBody.deleteRow(rowLength - 2);
   //Calles object method to populate daily sales arrays
   tempStore.salesGen();
   //calls render method to append td elements to table
   tempStore.render();
+  dropDownTime(tempStore.storeLocation, objectCounter, 'location');
+  objectCounter+= 1;
   //recreates total row with new values added
   totalGen();
   form.reset();//Resets form input fields
 }
+
+function changeCell(e) {
+  e.preventDefault();
+  var rowValue = event.target.location.value;
+  var colValue = event.target.time.value;
+  var newCell = event.target.new_value.value;
+  var selectedRow = tableBody.getElementsByTagName('tr')[parseInt(rowValue)];
+  console.log(selectedRow);
+  //console.log(newCell);
+  form.reset();
+}
+
+
+
+
+
+
+
 //Add's event listener to form that runs createStore function
 form.addEventListener('submit', createStore);
+cellForm.addEventListener('submit', changeCell);
